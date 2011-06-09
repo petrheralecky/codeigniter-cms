cdeea15361b19aa30a9067edeaa920cf1ddda38b
Petr Heralecký <heralecky@propeople.cz>
2011-06-09 22:22:32 +0200

    set

--- app/core/MY_Controller.php
+++ app/core/MY_Controller.php
@@ -17,7 +17,7 @@ class MY_Controller extends BASE_Controller {
 	public function __construct(){
 		parent::__construct();
 
-		Form::$img_app_path = FORM_IMG_APP_PATH;
+
 
 		$this->data['settings'] = $this->settings_model->get_all();
 		$this->data['controller'] = $this->uri->rsegment(1);
@@ -39,48 +39,8 @@ class MY_Controller extends BASE_Controller {
 		$this->data['view_content'] = ""; // alternative view
 		$this->data['view_render'] = ""; // alternative path to view
 		$this->uri->save_history($this->uri->uri_string(),$this->uri->rsegment_array());
-
-		// login
-		$f = new Form("login");
-		if($f->ready()){
-			$data = $f->get_data();
-			if($this->users_model->login($data)){
-				Ses::user($this->users_model->data);
-				Tools::flash("Byl jste úspěšně přihlášen","succ");
-				$this->uri->redirect("");
-			}else{
-				Tools::flash("Zadal jste špatný login nebo heslo","alert");
-			}
-		}
-
-		$this->data['f_login'] = $f;
-
-
-		// title
-		$this->data['title'] = "Direct mail :: Propeople";
-		$this->data['description'] = "";
-
-		$this->data['user'] = Ses::user_array();
-
-		// act
-		$this->load->model('act_model');
-		$this->data['acts'] = $this->act_model->get_all();
-
-		// files
-		$this->load->model("files_model");
-		$this->data['files'] = $this->files_model->get_all();
-
-		// links
-		$this->load->model("base_model","links");
-		$this->links->load(0,0,"links");
-		$this->data['links'] = $this->links->get_all();
-
-		// subsites
-		$this->data['sites'] = $this->sites_model->get_all();
-		if(SERVER=="local" && $this->data['controller']!="admin"){
-			$this->output->enable_profiler();
-		}
-		
+	
+		$this->init();
 	}
 
 }
--- app/views/pieces/contact-form.php
+++
@@ -1,9 +0,0 @@
-<? $f->start("","id='kontakt-form'"); ?>
-	<h2>Kontaktní formulář:</h2>
-	<table class="my-table">
-		<tr><th>Jméno/firma:</th><td><? $f->_('<input type="text" name="jmeno" />',1); ?></td></tr>
-		<tr><th>Kontakt:</th><td><? $f->_('<input type="text" name="kontakt" />'); ?></td></tr>
-		<tr><td colspan="2"><? $f->_('<textarea name="vzkaz"></textarea>',1); ?></td></tr>
-		<tr><td colspan="2"><? $f->_('<input class="sub" type="submit" name="ok_vzkaz" value="Odeslat" />'); ?></td></tr>
-	</table>
-<? $f->stop(); ?>
